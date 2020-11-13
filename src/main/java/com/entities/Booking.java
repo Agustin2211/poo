22831ffcf4beb377467d4624d04ca2a75eebbcb4
MarceloMr83 <marcleomrios83@gmail.com>
2020-11-13 00:20:45 -0300

    creadas anotaciones de relacion y columna

--- src/main/java/com/entities/Booking.java
+++ src/main/java/com/entities/Booking.java
@@ -1,24 +1,65 @@
 package com.entities;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+//import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+//import java.util.ArrayList;
 import java.util.Date;
+//import java.util.List;
 
 @Entity
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
+
+    @OneToOne
+    @JoinColumn(name = "id", updatable = false, nullable = false)
     private User guest;
+
+    @Column(name = "checkIn", nullable = false)
     private Date checkIn;
+
+    @Column(name = "checkOut", nullable = false)
     private Date checkOut;
+
+    @Column(name = "createdAt", nullable = false)
     private Date createdAt;
+
+    //solo puede reservar una habitacion?
+   
+    @OneToOne
+    @JoinColumn(name = "id", updatable = false, nullable = false)
     private Room room;
+    
+    //si se puede reservar mas de una habitacion
+   /* @OneToMany(mappedBy="Booking" )
+    private List<Room> listRoom;*/
+
+    @Column(name = "breakfastIncluded", nullable = false)
     private boolean breakfastIncluded;
+
+    @Column(name = "parking", nullable = false)
     private boolean parking;
+
+    @Column(name = "freeCancelation", nullable = false)
     private boolean freeCancelation;
+
+    @Column(name = "cost", nullable = true)
     private float cost;
+
+    public Booking(){
+            super();
+          //  listRoom=new ArrayList<Room>();
+          }
+    
 
     public long getId() {
         return id;
