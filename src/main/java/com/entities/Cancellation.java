000cddb3e65d8c1104bdaff129444c4adf1bd71f
MarceloMr83 <51954324+MarceloMr83@users.noreply.github.com>
2020-11-13 00:56:19 -0300

    Add files via upload
    Creadas las anotaciones de nombre de columna y de relacion

---
+++ src/main/java/com/entities/Cancellation.java
@@ -0,0 +1,54 @@
+package com.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.JoinColumn;
+import java.util.Date;
+
+@Entity
+public class Cancellation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "createdAt", nullable = false)
+    private Date createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "id", updatable = false, nullable = false)
+    private Booking booking;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+    @Override
+	public String toString() {
+		return "User [id=" + id + ", createdAt=" + createdAt.toString() + ", booking=" + booking.toString() +"]";
+	}   
+
+}
